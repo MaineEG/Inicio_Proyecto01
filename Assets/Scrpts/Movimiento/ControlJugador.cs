9a1e0b9a12322e39f18c081f3486e75521df2c3d
MaineEG <mychemical977@gmail.com>
2023-03-14 14:51:38 +0600

    Prueba
    Prueba

--- Assets/Scrpts/Movimiento/ControlJugador.cs
+++ Assets/Scrpts/Movimiento/ControlJugador.cs
@@ -8,11 +8,13 @@ public class ControlJugador : MonoBehaviour
     // Obtener entradas del jugador
 
     private Movimiento movimiento;
-    private Vector2 entradaControlMov; 
+    private Vector2 entradaControlMov;
+    private LanzaProyectiles LanzaProyectiles;
 
     void Start()
     {
         movimiento = GetComponent<Movimiento>();
+        LanzaProyectiles = GetComponent<LanzaProyectiles>();
     }
 
     
@@ -23,11 +25,19 @@ public class ControlJugador : MonoBehaviour
 
     public void AlMoverse (InputAction.CallbackContext context)
     {
+        movimiento.VoltearTransform(entradaControlMov.x);
         entradaControlMov = context.ReadValue<Vector2>();
     }
 
     public void AlSaltar(InputAction.CallbackContext context)
     {
         movimiento.Saltar(context.action.triggered);
+    }
+
+    public void AlLanzar(InputAction.CallbackContext context)
+    {
+        if (!context.action.triggered) { return; }
+        LanzaProyectiles.Lanzar();
+
     }
 }
